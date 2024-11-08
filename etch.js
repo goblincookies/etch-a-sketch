8ed8ec6b621e55927692ff60b03289a887520713
goblincookies <goblincookiebox@gmail.com>
2024-11-07 22:13:15 -0500

    completed project with comments

--- etch.js
+++ etch.js
@@ -10,7 +10,6 @@ let colorsAll = [ "#000000","#7c7c7c","#bcbcbc","#f8f8f8","#0000fc","#0078f8",
 let opacityAll = [];
 let indexColor = 0;
 let indexOpacity = 0;
-// let opacitySelectedIndex = 0;
 let colorPicker = document.querySelector("#color-list");
 let colorList = colorPicker.querySelectorAll("div");
 let canvas = document.querySelector("#canvas");
@@ -25,27 +24,19 @@ let rainbowMode = false;
 let alwaysDraw = false;
 let mouseDown = 0;
 
-
-document.body.onmousedown = function() { 
-  mouseDown=1;
-}
-
-document.body.onmouseup = function() {
-  mouseDown=0;
-}
-
+// KEEP TRACK OF MOUSE FOR DRAG-DRAWING
+document.body.onmousedown = () => mouseDown=1;
+document.body.onmouseup = () => mouseDown=0;
 
 toggleRainbow.addEventListener("click", () => rainbowMode = !rainbowMode );
 toggleAlwaysDraw.addEventListener("click", () => alwaysDraw = !alwaysDraw );
-// gridInput.addEventListener("input", changingGrid);
 gridInput.addEventListener("focusout", changingGrid);
 gridInput.addEventListener("keydown", changingGridKey);
+document.querySelector("#clear-grid").addEventListener("click", clearGrid );
 
 
-
-
+// DRAW THE PALETTE
 function setColors() {
-    // let colorList = colorPicker.querySelectorAll("div");
     let index = 0
     colorList.forEach( element => {
         if (index < colorsAll.length) {
@@ -59,6 +50,7 @@ function setColors() {
     updateOpacity(colorsAll[indexColor]);
 };
 
+// DRAW THE OPACITY PALETTE
 function updateOpacity( color ) {
     let opacityList = opacityPicker.querySelectorAll("div");
     let rgbColor = hexToRgb(color);
@@ -95,6 +87,7 @@ function updateOpacity( color ) {
 
 };
 
+// TRIGGERS WHEN A COLOR IS SELECTED
 function selectColor( e ) {
     // REMOVE VISUAL INDICATORS
     colorList.forEach( element => {
@@ -110,6 +103,7 @@ function selectColor( e ) {
     updateOpacity( e.target.id );
 };
 
+// TRIGGERS WHEN AN OPACITY IS SELECTED
 function selectOpacity( e ) {
     let opacityList = opacityPicker.querySelectorAll("div");
 
@@ -128,13 +122,9 @@ function selectOpacity( e ) {
 };
 
 // CONVERT HEX TO RGB
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    // return result ? {
-    //     r: parseInt(result[1], 16),
-    //     g: parseInt(result[2], 16),
-    //     b: parseInt(result[3], 16)
-    // } : null;
     return result ? [
         parseInt(result[1], 16),
         parseInt(result[2], 16),
@@ -142,58 +132,42 @@ function hexToRgb(hex) {
     ] : null;
 };
 
+// CONVERT R/G/B/A component TO HEX
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 function componentToHex(c) {
     var hex = c.toString(16);
     hex = hex.split(".")[0];
     return hex.length == 1 ? "0" + hex : hex;
 };
-  
+
+// CONVERT RGBA TO HEX
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 function rgbToHex(r, g, b, a) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b) + componentToHex(a*255);
 };
+
+// TRIGGERS WHEN A CANVAS BLOCK IS CLICKED
 function blockClick(e) {
-    e.target.style.backgroundColor = selectedColor;
+    draw( e.target, selectedColor);
 };
 
+// TRIGGERS WHEN A CANVAS BLOCK IS HOVERED
 function blockHover(e){
-    // console.log(`hovering ${selfIndex-gridRes}`);
     if (alwaysDraw | mouseDown ) {
-        e.target.style.backgroundColor = selectedColor;
+        draw(e.target, selectedColor);
     }
-    
-    // REMOVE ALL FIRST
-    blocksAll.forEach( element => {
-        element.classList.remove("block-side");
-    });
-    
-    // ADD BLOCK LINES
-    let selfIndex = Array.from( e.target.parentNode.childNodes).indexOf(e.target); //e.target.parentNode.childNodes.indexOf(e.target)
-    
-    if (e.target.previousSibling) {
-        e.target.previousSibling.classList.add("block-side");
-    }
-    
-    if (e.target.nextSibling ) {
-        e.target.nextSibling.classList.add("block-side");
-    }
-    
-    
-    if( selfIndex - gridRes > 0 ) {
-        // let topNode = e.target.parentNode.childNodes[selfIndex - xWidth];
-        Array.from( e.target.parentNode.childNodes)[selfIndex - gridRes].classList.add("block-side");
-        console.log("hellow");
-    }
-    if( selfIndex + gridRes < gridRes*gridRes ) {
-        // let topNode = e.target.parentNode.childNodes[selfIndex - xWidth];
-        Array.from( e.target.parentNode.childNodes)[selfIndex + gridRes].classList.add("block-side");
-        console.log("hellow");
-    }
-
-    // Previous/Next block
-
-    // Above/Below block 
 };
 
+// DRAW THE COLOR TO THE BLOCK
+function draw( target, color) {
+    if (rainbowMode) {
+        target.style.backgroundColor = colorsAll[ Math.floor(Math.random()*colorsAll.length)];
+    } else {
+        target.style.backgroundColor = color;
+    }
+}
+
+// TRIGGERS WHEN YOU UN-FOCUS THE GRID INPUT
 function changingGrid( e ) {
     let val = Math.min( 25, Math.max(16, e.target.value ) );
     if (val != e.target.value ) {
@@ -201,8 +175,9 @@ function changingGrid( e ) {
     };
     establishGrid( val ) ;
 }
-function changingGridKey( e ) {
 
+// TRIGGERS WHEN YOU PRESS "ENTER" ON THE GRID INPUT
+function changingGridKey( e ) {
     if (e.key === 'Enter') {
         let val = Math.min( 25, Math.max(16, e.target.value ) );
         if (val != e.target.value ) {
@@ -213,10 +188,18 @@ function changingGridKey( e ) {
     };
 };
 
+// TRIGGERS WHEN YOU CLICK "CLEAR GRID"
+function clearGrid() {
+    clearGrid = true;
+    establishGrid( gridRes );
+};
+
+// ERASES AND DRAWS THE CANVAS GRID
 function establishGrid( val ) {
     console.log("new grid: " + val);
 
-    if (gridRes != val ){
+    if (gridRes != val | clearGrid ){
+        clearGrid = false;
         gridRes = val;
         let xWidth = canvas.offsetWidth / val;
     
@@ -230,7 +213,7 @@ function establishGrid( val ) {
     
         for (let x = 0; x < val*val; x++ ) {
             const xDiv = document.createElement("div"); 
-            xDiv.classList.add("block");
+            xDiv.classList.add("block", "non", "grid-draw");
             xDiv.style.cssText=`width: ${xWidth}px;height: ${xWidth}px;`;
             xDiv.addEventListener("click", blockClick);
             xDiv.addEventListener("mouseover", blockHover);
@@ -238,15 +221,7 @@ function establishGrid( val ) {
             canvas.appendChild(xDiv);
         };
     };
-
-    // for (let y = 0; y < val; y++) {
-    //     const hDiv = document.createElement("div");
-    //     hDiv.classList.add("grid-row");
-        
-    //     canvas.appendChild(hDiv);
-    // };
 };
 
 setColors();
 establishGrid(16);
-// updateOpacity("#a4e4fc");
